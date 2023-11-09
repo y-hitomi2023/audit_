e2dbee4ad2d456a46c88ffc32a611f79bb9a0ad7
y-hitomi2023 <as8stream@gmail.com>
2023-11-09 17:56:50 +0900

    ヘッダーの修正

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,16 +5,9 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
+                    <a href="{{ route('root') }}">
                         <x-application-logo class="block h-9 w-auto fill-current text-gray-800" />
                     </a>
-                </div>
-
-                <!-- Navigation Links -->
-                <div class="hidden space-x-8 sm:-my-px sm:ml-10 sm:flex">
-                    <x-nav-link :href="route('dashboard')" :active="request()->routeIs('dashboard')">
-                        {{ __('Dashboard') }}
-                    </x-nav-link>
                 </div>
             </div>
 
@@ -44,7 +37,7 @@
 
                     <x-slot name="content">
                         @auth
-                            <x-dropdown-link :href="route('posts.create')">
+                            <x-dropdown-link :href="route('audios.create')">
                                 {{ __('Create Post') }}
                             </x-dropdown-link>
                             <form method="POST" action="{{ route('logout') }}">
@@ -105,7 +98,7 @@
                     </x-responsive-nav-link>
 
                     <!-- Authentication -->
-                    <x-responsive-nav-link :href="route('posts.create')">
+                    <x-responsive-nav-link :href="route('audios.create')">
                         {{ __('Create Post') }}
                     </x-responsive-nav-link>
                     <form method="POST" action="{{ route('logout') }}">
